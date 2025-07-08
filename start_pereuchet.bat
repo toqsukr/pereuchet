52c544b99c3657cb66a14ff51916f334236e07f9
Wastard <Ilya.pakylov@gmail.com>
2025-07-08 23:16:43 +0300

    added as admin start_pereuchet execution

--- start_pereuchet.bat
+++ start_pereuchet.bat
@@ -1,2 +1,12 @@
 @echo off
+:: Проверяем, запущен ли скрипт от администратора
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo You must be admin...
+    :: Перезапускаем скрипт с правами админа
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~dpnx0' -Verb RunAs"
+    exit /b
+)
+
+:: Если уже админ — выполняем команды
 start wsl -d Ubuntu -- bash -c "sudo service docker start && cd ~/pereuchet && sudo docker compose up -d; exec bash"
